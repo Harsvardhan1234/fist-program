4099c2b937b761aab1c0ec92c96538af2fa6a64f
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-09-12 22:14:00 +0530

    Create division od two integer.c

---
+++ division od two integer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+    int a, b, result;
+
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+  
+    result = a / b;  
+
+
+    printf("Division of %d by %d = %d\n", a, b, result);
+}
+  
