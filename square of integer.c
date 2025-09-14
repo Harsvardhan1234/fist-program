04de4defb8cf0e373182b75ae12bb3fed4b40b46
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-09-14 15:44:23 +0530

    Create square of integer.c

---
+++ square of integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+    int a, square;
+
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    square = a * a;
+
+    printf("Square of %d = %d\n", a, square);
+
+}
